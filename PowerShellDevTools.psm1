126214ca3505d21e12880930abcbad6447ee55d0
Attila Csombok <Attila_Csombok@epam.com>
2018-01-29 13:12:38 +0100

    Initial commit

---
+++ PowerShellDevTools.psm1
@@ -0,0 +1,277 @@
+﻿function Get-Parent
+{
+    param
+    (
+        [ValidateScript({ (Test-Path -Path $_ -PathType Leaf) })]
+        [string] $FilePath,
+        [ValidateSet('sln', 'csproj')]
+        [string] $Extension
+    )
+    
+    $fileName = Get-ChildItem -Path $FilePath | Select -ExpandProperty Name
+    $targetFolder = Split-Path -Path $FilePath
+    $folderPath = $targetFolder
+    while(-not [string]::IsNullOrWhiteSpace($folderPath) -and (Test-Path -Path $folderPath -PathType Container))
+    {
+        $cotainers = Get-ChildItem -Path $folderPath -Filter "*.$Extension"
+        $cotainers | ForEach-Object {
+            $content = Get-Content -Path $_.FullName
+            if(($content | Select-String -Pattern $fileName | Measure-Object).Count -gt 0)
+            {
+                return $_.FullName
+            }
+        }
+
+        $folderPath = Split-Path $folderPath -ErrorAction SilentlyContinue        
+    }
+}
+
+function Get-MatchingSources
+{
+    param
+    (         
+         [string] $Path = (Get-Location).Path,
+         [array] $Patterns,
+         [array] $Extension = ('*.cs', '*.h', '*.cpp')
+    )
+
+    $files = Get-ChildItem -Path $Path -Include $Extension -Recurse -File
+    $files | ForEach-Object {
+        $filePath = $_.FullName
+        $content = Get-Content -Path $filePath
+
+        $mismatch = $Patterns | 
+            Where-Object {
+                ($content | Select-String -Pattern $_ | Measure-Object).Count -eq 0
+            } |
+            Measure-Object |
+            Select-Object -ExpandProperty Count
+            
+
+        if($mismatch -eq 0)
+        {   
+            $project = Get-Parent -FilePath $filePath -Extension csproj
+            $solutions = if([string]::IsNullOrEmpty($project)) { '' } else { (Get-Parent -FilePath $project -Extension sln)}
+                                   
+            New-Object PSObject -Property @{ 
+                    File = $_.Name; 
+                    Path = $filePath; 
+                    Project = $project;
+                    Solutions = $solutions -join '; '
+                }                        
+        }
+    }    
+}
+
+function Get-NugetPackageInformation
+{
+    param
+    (
+        [string] $Path = (Get-Location).Path,
+        [string] $Id,
+        [string] $Version        
+    )
+
+   $packagesFiles = Get-ChildItem -Path $Path -Filter 'packages.config' -Recurse
+
+   $matches = $packagesFiles | ForEach-Object {
+        $fileName = $_.FullName
+        $doc = [xml](Get-Content -Path $fileName)
+        $doc.packages.package | 
+        Where-Object { $Id -eq $null -or $_.id -like "*$Id*"} |
+        Where-Object { $Version -eq $null -or $_.version -like "*$Version*"} |        
+        Select-Object id, version, targetFramework, @{Label="File"; Expression={$fileName}}
+   }
+
+   $matches | Sort-Object -Unique -Property id, version
+}
+
+function Get-PackageInformation
+{
+    param
+    (        
+        [ValidateScript({Test-Path $_ -PathType Container})]
+        $Path = (Get-Location).Path,
+        $Dependency        
+    )
+
+    $packages = @{}
+
+    $projectJsonFiles = Get-ChildItem -Path $Path -Filter 'project.json' -Recurse
+    $projectJsonFiles | ForEach-Object {  
+              
+        $filePath = $_.FullName        
+
+        try
+        {
+            $projectJSON = (Get-Content -Path $filePath) | Out-String | ConvertFrom-Json
+
+            $matchingDependencies = $projectJSON.dependencies.psobject.Properties | Where-Object {
+                    return [string]::IsNullOrEmpty($Dependency) -or $_.Name -like $Dependency
+            }
+            
+            if($matchingDependencies.Count -gt 0) {
+                Write-Host $filePath -ForegroundColor Cyan
+                $matchingDependencies | foreach {                    
+                    if($packages.ContainsKey($_.Name)) {
+                        $expectedVersion = $packages.Get_Item($_.Name)
+                        if($expectedVersion -ne $_.Value) {
+                            Write-Host "--> $($_.Name) - $($_.Value) (version mismatch, expected: $expectedVersion)" -ForegroundColor Red
+                        }
+                        else {
+                            Write-Host "--> $($_.Name) - $($_.Value)" -ForegroundColor Yellow
+                        }
+                    }
+                    else {
+                        Write-Host "--> $($_.Name) - $($_.Value)" -ForegroundColor Yellow
+                        $packages.Add($_.Name, $_.Value)
+                    }
+                }
+            }      
+        }
+        catch 
+        {
+            Write-Host "Invalid JSON file: $filePath" -ForegroundColor Red  
+        }  
+    }                
+}
+
+
+function Compare-DirectoryAssembly
+{
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_ -PathType Container})]
+        $SourceDirectory,
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_ -PathType Container})]
+        $DestinationDirectory,
+        $Filter,
+        [switch]
+        $CompareVersion,
+        [switch]
+        $IncludeEqual
+    )
+
+    $sourceAssemblies = Get-ChildItem -Path "$SourceDirectory*" -Recurse  -Filter "*$Filter*.dll"  | Select Name, @{Label="FileVersion"; Expression={[System.Diagnostics.FileVersionInfo]::GetVersionInfo($_.FullName).FileVersion}} -Unique
+    $destinationAssemblies = Get-ChildItem -Path "$DestinationDirectory*" -Recurse -Filter "*$Filter*.dll" | Select Name, @{Label="FileVersion"; Expression={[System.Diagnostics.FileVersionInfo]::GetVersionInfo($_.FullName).FileVersion}} -Unique
+
+    if($sourceAssemblies -eq $null) { $sourceAssemblies = @() }
+    if($destinationAssemblies -eq $null) { $destinationAssemblies = @() }
+
+    $compareProperties = @('Name')
+    if($CompareVersion.IsPresent) {$compareProperties+='FileVersion'}
+
+    Compare-Object -ReferenceObject $sourceAssemblies -DifferenceObject $destinationAssemblies -IncludeEqual:($IncludeEqual.IsPresent) -Property $compareProperties
+}
+
+function Compare-Npm
+{
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_ -PathType Leaf})]
+        $SourceNpm,
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path $_ -PathType Leaf})]
+        $DestinationNpm,
+        $Filter,
+        [switch]
+        $IncludeEqual,
+        [switch]
+        $IncludeDevDeps
+    )
+
+    $sourcePackages = Get-Content -Path $SourceNpm | ConvertFrom-Json
+
+    $destinationPackages = Get-Content -Path $DestinationNpm | ConvertFrom-Json
+
+    $result = @()
+
+    if($sourcePackages.dependencies) {
+        $sourcePackages.dependencies.PSObject.Properties | foreach {
+            $packageItem = New-Object PSObject -Property @{            
+                Name       = $_.Name
+                Type       = 'dependencies'     
+                SourceVersion = $_.Value
+                DestinationVersion  = $null }
+
+            $result += $packageItem
+        }   
+    }
+
+    if($sourcePackages.devDependencies) {
+        $sourcePackages.dependencies.PSObject.Properties | foreach {
+            $packageItem = New-Object PSObject -Property @{            
+                Name       = $_.Name
+                Type       = 'devDependencies'     
+                SourceVersion = $_.Value
+                DestinationVersion  = $null }
+
+            $result += $packageItem
+        }   
+    }
+    
+
+    if($destinationPackages.dependencies) {
+        $destinationPackages.dependencies.PSObject.Properties | foreach {
+            $dependency = $_
+        
+            $existing = $result | Where-Object {$_.Name -eq $dependency.Name -and $_.Type -eq 'dependencies'} | Select -First 1
+
+            if($existing) {
+                $existing.DestinationVersion = $dependency.Value
+            } else {        
+                $packageItem = New-Object PSObject -Property @{            
+                    Name       = $_.Name
+                    Type       = 'dependencies'     
+                    SourceVersion = $null
+                    DestinationVersion  = $dependency.Value }
+
+                $result += $packageItem
+            }
+        }   
+    }
+
+        if($destinationPackages.devDependencies) {
+        $destinationPackages.devDependencies.PSObject.Properties | foreach {
+            $dependency = $_
+        
+            $existing = $result | Where-Object {$_.Name -eq $dependency.Name -and $_.Type -eq 'devDependencies'} | Select -First 1
+
+            if($existing) {
+                $existing.DestinationVersion = $dependency.Value
+            } else {        
+                $packageItem = New-Object PSObject -Property @{            
+                    Name       = $_.Name
+                    Type       = 'devDependencies'     
+                    SourceVersion = $null
+                    DestinationVersion  = $dependency.Value }
+
+                $result += $packageItem
+            }
+        }   
+    }
+
+    if($IncludeEqual.IsPresent -and $IncludeDevDeps.IsPresent) {
+        $result | ft Name, Type, SourceVersion, DestinationVersion
+    } elseif($IncludeEqual.IsPresent -and -not $IncludeDevDeps.IsPresent) {
+        $result | Where-Object {$_.Type -eq 'dependencies'} | ft Name, Type, SourceVersion, DestinationVersion
+    } elseif(-not $IncludeEqual.IsPresent -and $IncludeDevDeps.IsPresent) {
+        $result | Where-Object {$_.SourceVersion -ne $_.DestinationVersion} | ft Name, Type, SourceVersion, DestinationVersion
+    } elseif(-not $IncludeEqual.IsPresent -and -not $IncludeDevDeps.IsPresent) { 
+        $result | Where-Object {$_.SourceVersion -ne $_.DestinationVersion -and $_.Type -eq 'dependencies'} | ft Name, Type, SourceVersion, DestinationVersion
+    }
+}
+
+
+Set-Alias -Name matchSources -Value Get-MatchingSources
+Set-Alias -Name listNuget -Value Get-NugetPackageInformation
+Set-Alias -Name listPackages -Value Get-PackageInformation
+Set-Alias -Name dllCompare -Value Compare-DirectoryAssembly
+Set-Alias -Name npmCompare -Value Compare-Npm
+
+
+Export-ModuleMember -Function * -Alias *
+
--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# powershell-dev-tools
-Productivity tools development
+# Overview
+PowerShell productivity tools for development
+
+# How to setup
+
+This tool is a self-contained PowerShell module. It can be used by explicitly installing to user's PowerShell module directory.
+
+[Please find details here how to install custom PowerShell modules.](https://msdn.microsoft.com/en-us/library/dd878350(v=vs.85).aspx)
+
+# Examples
+
+## Compare NPM dependecies
+
+This tool is suppused to compare *npm* dependencies in a source and a destination package.json. 
+By using this tool it's easy to higlight version mismatches.
+
+```PowerShell
+# Simple compare 
+npmCompare c:\examples\first\package.json c:\examples\second\package.json
+
+# Include DEV dependecies
+npmCompare c:\examples\first\package.json c:\examples\second\package.json -IncludeDevDeps
+
+# Show mathcing entries as well
+npmCompare c:\examples\first\package.json c:\examples\second\package.json -IncludeEquals
+```
+
+**Example output:**
+```
+Name                                          Type         SourceVersion DestinationVersion
+----                                          ----         ------------- ------------------
+@google-cloud/bigquery                        dependencies 1.0.0         ^0.9.6
+@google-cloud/datastore                       dependencies 1.1.0         1.0.4
+async                                         dependencies 2.6.0         2.3.0
+body-parser                                   dependencies 1.18.2
+cors                                          dependencies 2.8.4
+debug                                         dependencies 3.1.0         2.6.8
+es6-promise                                   dependencies 4.2.2
+express                                       dependencies 4.16.2
+helmet                                        dependencies 3.9.0
+parseurl                                      dependencies 1.3.2
+path-to-regexp                                dependencies 2.1.0
+serve-static                                  dependencies 1.13.1
+strict-transport-security                     dependencies 0.2.1
+uuid                                          dependencies 3.2.1         3.1.0
+```
+
+## Compare DLLs in source directories
+
+This tool is suppused to compare dlls in a source and a destination folders. 
+By using this tool it's easy to higlight version mismatches and missing references.
+
+```PowerShell
+# Simple compare 
+dllCompare c:\examples\first c:\examples\second\
+
+# Use filter
+dllCompare c:\examples\first c:\examples\second\ -Filter Microsoft*
+
+# Analyze file versions
+dllCompare c:\examples\first c:\examples\second\ -CompareVersion
+```
+
+**Example output:**
+
+```
+Name                              FileVersion  SideIndicator
+----                              -----------  -------------
+Hangfire.Core.resources.dll       1.6.8.0      =>
+Hangfire.Core.dll                 1.6.8.0      =>
+Hangfire.SqlServer.dll            1.6.8.0      =>
+Hangfire.SqlServer.dll            0.0.0.0      =>
+Microsoft.Owin.dll                3.0.30819.47 =>
+Microsoft.Owin.Host.SystemWeb.dll 3.0.30819.47 =>
+Newtonsoft.Json.dll               5.0.1.16007  =>
+Owin.dll                          1.0          =>
+```
